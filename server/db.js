96048eca36e671fb08637107f7093ebf022b2d83
mouiz ahmed <mouiza@my.yorku.ca>
2023-05-18 22:32:09 -0400

    migrated server to planetscale NoSQL

--- server/db.js
+++ server/db.js
@@ -4,13 +4,16 @@ import * as dotenv from 'dotenv'
 
 const config = dotenv.config().parsed;
 
-export const db = mysql.createConnection({
-    host: config.HOST,
-    user: config.USER,
-    password: config.PASSWORD,
-    database: config.DATABASE,
-    multipleStatements: true
-});
+// export const db = mysql.createConnection({
+//     host: config.HOST,
+//     user: config.USER,
+//     password: config.PASSWORD,
+//     database: config.DATABASE,
+//     ssl: config.ssl,
+//     multipleStatements: true
+// });
+
+export const db = mysql.createConnection(config.DATABASE_URL);
 
 db.connect((err) => {
     if (err) {
